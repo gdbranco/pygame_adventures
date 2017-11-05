a32069f0ee58eb6e24dd1d06920b50bd82cedd5c
gdbranco <gdbranco@gmail.com>
2017-11-05 16:07:17 -0200

    Better snake, refactoring, TODO: sprites

--- Nibbles/main.py
+++ Nibbles/main.py
@@ -3,221 +3,108 @@ Author: Guilherme David Branco
 NIBBLE/SNAKE CLONE GAME to learn PYGAME
 """
 import random
-import sys
 import pygame
-import time
+from snake import Snake
+from collections import deque
 from pygame.locals import *
+from config import *
+
+class SnakeRun(object):
+    def __init__(self):
+        pygame.display.set_caption("SNAKE CLONE")
+        self.block_size = BLOCK_SIZE
+        self.window = pygame.display.set_mode((800,600))
+        self.screen = pygame.display.get_surface()
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font("./waffle.otf", 20)
+        self.world = Rect((0,0), WORLD_SIZE)
+        self.playing = START_MENU
+
+    def reset(self):
+        self.playing = PLAYING
+        self.next_direction = DIRECTION_UP
+        self.score = 0
+        self.snake = Snake(self.world.center, SNAKE_INIT_LENGTH)
+        self.food = set()
+        self.add_food()
+    
+    def add_food(self):
+        while not (self.food and random.randrange(4)):
+            food = Vector(map(random.randrange, self.world.bottomright))
+            if food not in self.food and food not in self.snake:
+                self.food.add(food)
+    
+    def handle_input(self, event):
+        if event.key in KEY_DIRECTION:
+            self.next_direction = KEY_DIRECTION[event.key]
+        elif event.key == K_SPACE and (self.playing == START_MENU or not self.playing):
+            self.reset()
+
+    def update(self, dt):
+        self.snake.update(dt, self.next_direction)
+        head = self.snake.head()
+        if head in self.food:
+            self.food.remove(head)
+            self.add_food()
+            self.snake.grow()
+            self.score += len(self.snake) * BASE_SCORE
+
+        if self.snake.intersect() or not self.world.collidepoint(self.snake.head()):
+            self.playing = 0
+
+    def draw_text(self, text, p):
+        self.screen.blit(self.font.render(text, 1, TEXT_COLOR), p)
+
+    def get_rect(self, p):
+        return pygame.Rect(p[0]*BLOCK_WIDTH, p[1]*BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT)
+
+    def draw_grid(self):
+        for x in range(0, SCREEN_WIDTH, BLOCK_WIDTH):
+            pygame.draw.line(self.screen, TEXT_COLOR, (x, 0), (x, SCREEN_HEIGHT))
+        for y in range(0, SCREEN_HEIGHT, BLOCK_HEIGHT):
+            pygame.draw.line(self.screen, TEXT_COLOR, (0, y), (SCREEN_WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(COLOR_BG)
+        self.draw_grid()
+        for p in self.snake:
+            pygame.draw.rect(self.screen, COLOR_SNAKE_OUT, self.get_rect(p))
+            pygame.draw.rect(self.screen, COLOR_SNAKE_IN, pygame.Rect(p[0]*BLOCK_WIDTH+ 4, p[1]*BLOCK_HEIGHT + 4, 
+                             BLOCK_WIDTH - 8, BLOCK_HEIGHT - 8))
+        for f in self.food:
+            pygame.draw.rect(self.screen, COLOR_APPLE, self.get_rect(f))
+    
+    def draw_start(self):
+        self.screen.fill(COLOR_BG)
+        self.draw_text("SNAKE CLONE", (SCREEN_WIDTH/2 - 150, SCREEN_HEIGHT/2))
 
 
-####CONSTANTS
-##GAME
-FPS = 60
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
-WINDOW_CENTER = WINDOW_WIDTH/2, WINDOW_HEIGHT/2
-CELL_SIZE = 25
-CELL_WIDTH = int(WINDOW_WIDTH/CELL_SIZE)
-CELL_HEIGHT = int(WINDOW_HEIGHT/CELL_SIZE)
+    def draw_gameover(self):
+        self.screen.fill(DEATH_COLOR)
+        self.draw_text("Game over! Aperte espaco para novo jogo", (SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/2))
+        self.draw_text("Seu score foi de {}".format(self.score), (SCREEN_WIDTH/2 - 200, SCREEN_HEIGHT/2 + 50))
 
-##COLORS
-COLORS = {
-    'white':      (255, 255, 255),
-    'black':      (0, 0, 0),
-    'red':        (255, 0, 0),
-    'green':      (0, 255, 0),
-    'blue':       (0, 0, 255),
-    'dark_green': (0, 155, 0),
-    'dark_gray':  (40, 40, 40),
-}
-BGCOLOR = COLORS['black']
-##MOVEMENT
-UP = 'up'
-DOWN = 'down'
-LEFT = 'left'
-RIGHT = 'right'
-##SNAKE HEAD
-HEAD = 0
-DIRECTION = None
-APPLE = None
-WORM = None
-SCORE = 0
-def endGame():
-    pygame.quit()
-    sys.exit()
-
-def checkKeyPress():
-    if len(pygame.event.get(pygame.QUIT)) > 0:
-        endGame()
-    keyUpEvents = pygame.event.get(KEYUP)
-    if len(keyUpEvents) == 0:
-        return None
-    if keyUpEvents[0].key == K_ESCAPE:
-        endGame()
-    pygame.event.get()
-    return keyUpEvents[0].key
-def clearScreen():
-    DISPLAYSURF.fill(BGCOLOR)
-
-def drawPressKey(inverte):
-    pressFont = pygame.font.Font('./waffle.otf', 30)
-    pressKeySurf = pressFont.render('Pressione qualquer botão para começar',
-                                    True, COLORS['green'] if inverte else COLORS['black'])
-    pressKeyRect = pressKeySurf.get_rect()
-    pressKeyRect.x = WINDOW_WIDTH/2 - pressKeyRect.width/2
-    pressKeyRect.y = WINDOW_HEIGHT/2 - pressKeyRect.height/2 + 100
-    DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
-
-def showGameOver():
-    print("GAMEOVER SCREEN")
-    titleFont = pygame.font.Font("./waffle.otf", 100)
-    titleSurf = titleFont.render('GAME OVER', True, COLORS['red'])
-    titleRect = titleSurf.get_rect()
-    X = 0 - titleRect.width
-    Y = WINDOW_HEIGHT/2 - titleRect.height/2
-    inverte = False
-    while True:
-        clearScreen()
-        DISPLAYSURF.blit(titleSurf, (X, Y))
-        drawPressKey(inverte)
-        pygame.display.update()
-        FPSCLOCK.tick(FPS)
-        if X > (WINDOW_WIDTH + titleRect.width/2):
-            X = -titleRect.width
-        X += 15
-        if X % 8 == 0:
-            inverte = not inverte
-        if checkKeyPress():
-            resetGame()
-            return
-
-def showStart():
-    print("START SCREEN")
-    titleFont = pygame.font.Font("./waffle.otf", 100)
-    titleSurf = titleFont.render('Snake Clone', True, COLORS['white'])
-    titleRect = titleSurf.get_rect()
-    X = 0 - titleRect.width
-    Y = WINDOW_HEIGHT/2 - titleRect.height/2
-    inverte = False
-    while True:
-        clearScreen()
-        DISPLAYSURF.blit(titleSurf, (X, Y))
-        drawPressKey(inverte)
-        pygame.display.update()
-        FPSCLOCK.tick(FPS)
-        if X > (WINDOW_WIDTH + titleRect.width/2):
-            X = -titleRect.width
-        X += 15
-        if X % 8 == 0:
-            inverte = not inverte
-        if checkKeyPress():
-            return
-
-def getRandomLocation():
-    x = random.randint(0, CELL_SIZE - 1)
-    y = random.randint(0, CELL_SIZE - 1)
-    return [x, y]
-
-def handleEvents():
-    global DIRECTION
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                endGame()
-            elif event.type == KEYDOWN:
-                if(event.key == K_LEFT or event.key == K_a) and DIRECTION != RIGHT:
-                    DIRECTION = LEFT
-                elif(event.key == K_RIGHT or event.key == K_d) and DIRECTION != LEFT:
-                    DIRECTION = RIGHT
-                elif(event.key == K_UP or event.key == K_w) and DIRECTION != DOWN:
-                    DIRECTION = UP
-                elif(event.key == K_DOWN or event.key == K_s) and DIRECTION != UP:
-                    DIRECTION = DOWN
-def drawGrid():
-    for x in range(0, WINDOW_WIDTH, CELL_WIDTH):
-        pygame.draw.line(DISPLAYSURF, COLORS['white'], (x, 0), (x, WINDOW_HEIGHT))
-    for y in range(0, WINDOW_HEIGHT, CELL_HEIGHT):
-        pygame.draw.line(DISPLAYSURF, COLORS['white'], (0, y), (WINDOW_WIDTH, y))
-def resetGame():
-    global DIRECTION
-    global APPLE
-    global WORM
-    APPLE = getRandomLocation()
-    WORM = []
-    WORM.insert(0,getRandomLocation())
-    clearScreen()
-    DIRECTION = None
-def runGame():
-    global DIRECTION
-    global APPLE
-    global WORM
-    print("RUNNING GAME")
-    resetGame()
-    while True:
-        clearScreen()
-        handleEvents()
-        # drawGrid()
-        moveWORM(WORM)
-        if checkEating(WORM,APPLE):
-            APPLE = getRandomLocation()
-        else:
-            del WORM[-1]
-        if checkBounds(WORM):
-            showGameOver()
-        drawAPPLE(APPLE)
-        drawWORM(WORM)
-        pygame.display.update()
-        FPSCLOCK.tick(FPS/6)
-
-def moveWORM(WORM):
-    global DIRECTION
-    if DIRECTION == UP:
-        newHead = [WORM[HEAD][0], WORM[HEAD][1] - 1]
-    elif DIRECTION == DOWN:
-        newHead = [WORM[HEAD][0], WORM[HEAD][1] + 1]
-    elif DIRECTION == LEFT:
-        newHead = [WORM[HEAD][0] - 1, WORM[HEAD][1]]
-    elif DIRECTION == RIGHT:
-        newHead = [WORM[HEAD][0] + 1, WORM[HEAD][1]]
-    elif DIRECTION is None:
-        newHead = [WORM[HEAD][0], WORM[HEAD][1]]
-    WORM.insert(0, newHead)
-
-def checkEating(WORM, APPLE):
-    if WORM[HEAD][0] == APPLE[0] and WORM[HEAD][1] == APPLE[1]:
-        return True
-    return False
-
-def checkBounds(WORM):
-    if WORM[HEAD][0] < 0 or WORM[HEAD][0] > CELL_SIZE - 1 or WORM[HEAD][1] < 0 or WORM[HEAD][1] > CELL_SIZE - 1:
-        return True
-    for segment in WORM[1:]:
-        if segment[0] == WORM[HEAD][0] and segment[1] == WORM[HEAD][1]:
-            return True
-    return False
-
-def drawWORM(WORM):
-    for coord in WORM:
-        x = coord[0]*CELL_WIDTH
-        y = coord[1]*CELL_HEIGHT
-        WORMRect = pygame.Rect(x, y, CELL_WIDTH, CELL_HEIGHT)
-        pygame.draw.rect(DISPLAYSURF, COLORS['dark_green'], WORMRect)
-        WORMInnerRect = pygame.Rect(x + 4, y + 4, CELL_WIDTH - 8, CELL_HEIGHT - 8)
-        pygame.draw.rect(DISPLAYSURF, COLORS['green'], WORMInnerRect)
-
-def drawAPPLE(coord):
-    APPLERect = pygame.Rect(coord[0]*CELL_WIDTH,coord[1]*CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT)
-    pygame.draw.rect(DISPLAYSURF, COLORS['red'], APPLERect)
+    def play(self):
+        while True:
+            dt = self.clock.tick(FPS) / 1000.0
+            for e in pygame.event.get():
+                if e.type == QUIT:
+                    return
+                elif e.type == KEYDOWN:
+                    self.handle_input(e)
+            if self.playing == PLAYING:
+                self.update(dt)
+                self.draw()
+            elif self.playing == START_MENU:
+                self.draw_start()
+            else:
+                self.draw_gameover()
+            pygame.display.update()
 
 def main():
-    global FPSCLOCK, DISPLAYSURF, BASICFONT
     pygame.init()
-    FPSCLOCK = pygame.time.Clock()
-    DISPLAYSURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
-    pygame.display.set_caption('SNAKE CLONE')
-    showStart()
-    while True:
-        runGame()
+    SnakeRun().play()
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
-
---
+++ Nibbles/snake.py
@@ -0,0 +1,43 @@
+from collections import deque
+from config import *
+class Snake(object):
+    def __init__(self, start, length):
+        self.speed = SNAKE_SPEED
+        self.timer = 1.0 / self.speed
+        self.growth_pending = 0
+        self.direction = DIRECTION_UP
+        self.segments = deque([start - self.direction * i for i in range(SNAKE_INIT_LENGTH)])
+    
+    def __iter__(self):
+        return iter(self.segments)
+
+    def __len__(self):
+        return len(self.segments)
+
+    def change_direction(self, direction):
+        if self.direction != -direction:
+            self.direction = direction
+
+    def head(self):
+        return self.segments[0]
+    
+    def update(self, dt, direction):
+        self.timer -= dt
+        if self.timer > 0:
+            return
+        self.change_direction(direction)
+        self.timer += 1/self.speed
+        self.segments.appendleft(self.head() + self.direction)
+        if self.growth_pending > 0:
+            self.growth_pending -= 1
+        else:
+            self.segments.pop()
+
+    def grow(self):
+        self.growth_pending += 1
+        self.speed += SNAKE_INC_SPEED
+    
+    def intersect(self):
+        it = iter(self)
+        head = next(it)
+        return head in it
